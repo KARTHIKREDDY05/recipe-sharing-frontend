83acaf7864daadd1a95aa528e8e11870e79acc61
Abhinasree <134438093+KARTHIKREDDY05@users.noreply.github.com>
2025-03-08 21:48:44 +0530

    Create Recipes.js

---
+++ src/pages/Recipes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipes = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Recipes</h1>
+      <p>Browse through a variety of delicious recipes.</p>
+    </div>
+  );
+};
+
+export default Recipes;
