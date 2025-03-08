36dc37e5a58bd9d761268ae8944643346a96da19
Abhinasree <134438093+KARTHIKREDDY05@users.noreply.github.com>
2025-03-08 21:48:17 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to Recipe Sharing</h1>
+      <p>Explore and share amazing recipes with the world.</p>
+    </div>
+  );
+};
+
+export default Home;
